e1b17a1d24d15637710ae60f9f4f19aa7d227bc3
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-05-14 15:28:05 -0300

    adicionado exercicio04

--- ListaSolicitacaoDeInformacaoWFA/ListaSolicitacaoDeInformacaoWFA/Exercicio04.Designer.cs
+++ ListaSolicitacaoDeInformacaoWFA/ListaSolicitacaoDeInformacaoWFA/Exercicio04.Designer.cs
@@ -28,19 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(348, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite um número para saber sua tabuada";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(12, 46);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(254, 20);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(12, 99);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(185, 278);
+            this.txtResultado.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tabuada";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 60);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Apresentar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Exercicio04
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(824, 455);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label1);
             this.Name = "Exercicio04";
             this.Text = "Exercicio04";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
--- ListaSolicitacaoDeInformacaoWFA/ListaSolicitacaoDeInformacaoWFA/Exercicio04.cs
+++ ListaSolicitacaoDeInformacaoWFA/ListaSolicitacaoDeInformacaoWFA/Exercicio04.cs
@@ -16,5 +16,28 @@ namespace ListaSolicitacaoDeInformacaoWFA
         {
             InitializeComponent();
         }
+
+        int numero = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textao = "";
+              
+            for (int i = 0; i < 11; i++)
+            {
+                ArmazenarInformacao();
+                double tabuada = numero * i;
+                textao = textao + string.Format("{0} x {1} = {2}\r\n", numero , i, tabuada); 
+            }
+
+            
+
+           txtResultado.Text = textao;
+        }
+
+        private void ArmazenarInformacao()
+        {
+            numero = Convert.ToInt32(txtNumero.Text);
+            
+        }       
     }
 }
