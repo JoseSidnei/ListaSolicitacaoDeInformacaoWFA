7723831d96c1af84326ca1949834ee7f6df04cc1
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-05-17 14:38:24 -0300

    finalizado exercico11

--- ListaSolicitacaoDeInformacaoWFA/ListaSolicitacaoDeInformacaoWFA/Exercicio11.Designer.cs
+++ ListaSolicitacaoDeInformacaoWFA/ListaSolicitacaoDeInformacaoWFA/Exercicio11.Designer.cs
@@ -32,9 +32,11 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtDiaDeNascimento = new System.Windows.Forms.TextBox();
+            this.txtAnoDeNascimento = new System.Windows.Forms.TextBox();
+            this.txtMesDeNascimento = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -43,9 +45,10 @@
             this.button1.Location = new System.Drawing.Point(13, 390);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(188, 51);
-            this.button1.TabIndex = 0;
+            this.button1.TabIndex = 9;
             this.button1.Text = "Apresentar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -63,7 +66,7 @@
             this.label2.AutoSize = true;
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(13, 136);
+            this.label2.Location = new System.Drawing.Point(12, 194);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(167, 22);
             this.label2.TabIndex = 2;
@@ -74,44 +77,67 @@
             this.label3.AutoSize = true;
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(13, 74);
+            this.label3.Location = new System.Drawing.Point(12, 105);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(168, 22);
             this.label3.TabIndex = 3;
             this.label3.Text = "Mês de Nascimento";
             // 
-            // textBox1
+            // txtDiaDeNascimento
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(12, 38);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 4;
+            this.txtDiaDeNascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDiaDeNascimento.Location = new System.Drawing.Point(12, 38);
+            this.txtDiaDeNascimento.Name = "txtDiaDeNascimento";
+            this.txtDiaDeNascimento.Size = new System.Drawing.Size(100, 22);
+            this.txtDiaDeNascimento.TabIndex = 1;
             // 
-            // textBox2
+            // txtAnoDeNascimento
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(12, 161);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 5;
+            this.txtAnoDeNascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnoDeNascimento.Location = new System.Drawing.Point(11, 219);
+            this.txtAnoDeNascimento.Name = "txtAnoDeNascimento";
+            this.txtAnoDeNascimento.Size = new System.Drawing.Size(100, 22);
+            this.txtAnoDeNascimento.TabIndex = 3;
             // 
-            // textBox3
+            // txtMesDeNascimento
             // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(12, 99);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 6;
+            this.txtMesDeNascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMesDeNascimento.Location = new System.Drawing.Point(11, 130);
+            this.txtMesDeNascimento.Name = "txtMesDeNascimento";
+            this.txtMesDeNascimento.Size = new System.Drawing.Size(100, 22);
+            this.txtMesDeNascimento.TabIndex = 2;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(502, 81);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(346, 360);
+            this.txtResultado.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(502, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 22);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Resultado";
             // 
             // Exercicio11
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(860, 453);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtMesDeNascimento);
+            this.Controls.Add(this.txtAnoDeNascimento);
+            this.Controls.Add(this.txtDiaDeNascimento);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -129,8 +155,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtDiaDeNascimento;
+        private System.Windows.Forms.TextBox txtAnoDeNascimento;
+        private System.Windows.Forms.TextBox txtMesDeNascimento;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label4;
     }
 }
--- ListaSolicitacaoDeInformacaoWFA/ListaSolicitacaoDeInformacaoWFA/Exercicio11.cs
+++ ListaSolicitacaoDeInformacaoWFA/ListaSolicitacaoDeInformacaoWFA/Exercicio11.cs
@@ -12,20 +12,95 @@ namespace ListaSolicitacaoDeInformacaoWFA
 {
     public partial class Exercicio11 : Form
     {
-        /* 11. Solicite o dia, mês e ano de nascimento e apresente as seguintes informações:
+        /*   11 Solicite o dia, mês e ano de nascimento e apresente as seguintes informações:
 
              ● Quantidade de dias vivo;
              ● Quantidade de anos vivo;
              ● Quantidade de meses vivo;
              ● Quantidade de horas vivo;
              ● Quantidade de minutos vivo;
-             ● Quantidade de segundos vivo; */
+             ● Quantidade de segundos vivo;  */
 
         public Exercicio11()
         {
             InitializeComponent();
         }
 
+        int diaDeNascimento = 0, anoDeNascimento = 0, mesDeNascimento = 0;
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ArmazenarInforacao();           
+            double somaAnosVivo = 2018 - anoDeNascimento;
+            double somaDiasVivo = (2018 - anoDeNascimento) * 365;
+            double somaMesesVivo = (2018 - anoDeNascimento) * 12;
+            double somaHorasVivo = (24 * 365) * somaAnosVivo;
+            double somaMinutosVivo = 60 * somaHorasVivo;
+            double somaSegundosVivo = (3600 * 365) * somaAnosVivo;
+
+            string texto = String.Format("Anos vivo: {1}\r\nDias vivo: {0}\r\nMeses vivo: {2}\r\nHoras vivo: {3}\r\nMinutos vivo: {4}\r\nSegundos vivo: {5}",
+             somaDiasVivo, somaAnosVivo, somaMesesVivo, somaHorasVivo, somaMinutosVivo, somaSegundosVivo);
+            txtResultado.Text = texto;
+        }
+
+        private void ArmazenarInforacao()
+        {            
+            try
+            {
+                diaDeNascimento = Convert.ToInt32(txtDiaDeNascimento.Text);
+                if (diaDeNascimento <= 0)
+                {
+                    MessageBox.Show("Dia de nascimento deve ser maior que 0 !");
+                    txtDiaDeNascimento.Focus();
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Dia de nascimento deve conter apenas números inteiros !");
+                txtDiaDeNascimento.Focus();
+                return;
+            }
+
+            try
+            {
+                mesDeNascimento = Convert.ToInt32(txtMesDeNascimento.Text);
+                if (mesDeNascimento <= 0)
+                {
+                    MessageBox.Show("Mês de nascimento deve ser maior que 0 !");
+                     txtMesDeNascimento.Focus();
+                     return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Mês de nascimento deve conter apenas números inteiros !");
+                txtMesDeNascimento.Focus();
+                return;
+            }
+
+            try
+            {
+                anoDeNascimento = Convert.ToInt32(txtAnoDeNascimento.Text);
+                txtAnoDeNascimento.Focus();
+                return;
+            }
+            catch
+            {
+                MessageBox.Show("Ano de nascimento deve conter somente números inteiro !");
+                txtAnoDeNascimento.Focus();
+                return;
+            }
+
+        }
+
+
+
+
+
+
+        public string texto { get; set; }
+
+        public object somaHorasVivo { get; set; }
     }
 }
